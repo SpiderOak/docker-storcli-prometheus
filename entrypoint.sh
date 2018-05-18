87306706242af201895d67d88761fb853ece7493
David Hain <dhain@spideroak.com>
2018-05-18 14:03:16 -0500

    Fix storcli path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ usage() {
 [ $# -eq 2 ] || usage
 trap 'kill -TERM $child 2>/dev/null' SIGTERM
 while true; do
-    python /storcli.py > "$1.$$" &
+    python /storcli.py --storcli_path=/usr/sbin/storcli > "$1.$$" &
     child=$!; wait $child
     mv "$1.$$" "$1"
     sleep "$2" &
